f667eaedc347d2ceee38a9f102e2d79927994615
xedoc <anton.meshkov@gmail.com>
2013-02-26 13:45:39 +0200

    Install package

--- Ubiquitous/Properties/AssemblyInfo.cs
+++ Ubiquitous/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ubiquitous")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Multichat")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Ubiquitous")]
 [assembly: AssemblyProduct("Ubiquitous")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Xedoc 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
